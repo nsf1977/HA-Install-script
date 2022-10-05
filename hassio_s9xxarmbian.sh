1e3a7fd92ec2564d58e9d87ad24291ff7d8a319b
nsf1977 <34452549+nsf1977@users.noreply.github.com>
2022-10-05 23:38:27 +0100

    Update hassio_s9xxarmbian.sh

--- hassio_s9xxarmbian.sh
+++ hassio_s9xxarmbian.sh
@@ -14,8 +14,11 @@ set -o pipefail # Return exit status of the last command in the pipe that failed
 # ==============================================================================
 # GLOBALS
 # ==============================================================================
-readonly HOSTNAME="armhassio"
-readonly HA_INSTALLER="https://raw.githubusercontent.com/home-assistant/supervised-installer/master/installer.sh"
+readonly HOSTNAME="homeassistant"
+readonly OS_AGENT="os-agent_1.2.2_linux_aarch64.deb"
+readonly OS_AGENT_PATH="https://github.com/home-assistant/os-agent/releases/download/1.2.2/"
+readonly HA_INSTALLER="homeassistant-supervised.deb"
+readonly HA_INSTALLER_PATH="https://github.com/home-assistant/supervised-installer/releases/latest/download/"
 readonly REQUIREMENTS=(
   apparmor-utils
   apt-transport-https
@@ -42,7 +45,9 @@ update_hostname() {
     sed -i "s/${old_hostname}/${HOSTNAME}/g" /etc/hostname
     sed -i "s/${old_hostname}/${HOSTNAME}/g" /etc/hosts
     hostname "${HOSTNAME}"
+    echo ""
     echo "Hostname will be changed on next reboot: ${HOSTNAME}"
+    echo ""
   fi
 }
 
@@ -50,11 +55,15 @@ update_hostname() {
 # Installs all required software packages and tools
 # ------------------------------------------------------------------------------
 install_requirements() {
+  echo ""
   echo "Updating APT packages list..."
+  echo ""
+  apt-get --allow-releaseinfo-change update
   apt-get install software-properties-common
   apt-get update
-
+  echo ""
   echo "Ensure all requirements are installed..."
+  echo ""
   apt-get install -y "${REQUIREMENTS[@]}"
 }
 
@@ -62,7 +71,9 @@ install_requirements() {
 # Installs the Docker engine
 # ------------------------------------------------------------------------------
 install_docker() {
+  echo ""
   echo "Installing Docker..."
+  echo ""
   curl -fsSL https://get.docker.com | sh
 }
 
@@ -70,8 +81,15 @@ install_docker() {
 # Installs and starts Hass.io
 # ------------------------------------------------------------------------------
 install_hassio() {
-  echo "Installing Hass.io..."
-  curl -sL "${HA_INSTALLER}" | bash -s -- -m qemuarm-64
+  echo ""
+  echo "Installing Home Assistant..."
+  echo ""
+  apt-get update
+  apt-get install udisks2 wget -y
+  wget "${OS_AGENT_PATH}${OS_AGENT}"
+  dpkg -i "${OS_AGENT}"
+  wget "${HA_INSTALLER_PATH}${HA_INSTALLER}"
+  dpkg -i "${HA_INSTALLER}"
 }
 
 # ------------------------------------------------------------------------------
@@ -84,6 +102,17 @@ config_network_manager() {
     echo -e "\n[connection]";
     echo "wifi.clone-mac-address=preserve";
   } >> "/etc/NetworkManager/NetworkManager.conf"
+}
+
+# ------------------------------------------------------------------------------
+# Upgrade final
+# ------------------------------------------------------------------------------
+upgrade_final() {
+  echo ""
+  echo "Upgrade..."
+  echo ""
+  sudo apt update
+  sudo apt upgrade -y
 }
 
 # ==============================================================================
@@ -104,6 +133,7 @@ main() {
   config_network_manager
   install_docker
   install_hassio
+  upgrade_final
 
   # Friendly closing message
   ip_addr=$(hostname -I | cut -d ' ' -f1)
