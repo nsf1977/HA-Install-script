02129583e92b6cb0eaae4569ddbc28c0be44349d
nsf1977 <34452549+nsf1977@users.noreply.github.com>
2022-10-07 10:00:03 +0100

    Update and rename armbian_install_script to teste-armbian_install_script.sh

--- teste-armbian_install_script.sh
+++ teste-armbian_install_script.sh
@@ -1,11 +1,11 @@
-
-#######################################################################
-#######################################################################
-##                                                                   ##
-## THIS SCRIPT SHOULD ONLY BE RUN ON A TANIX TX3 BOX RUNNING ARMBIAN ##
-##                                                                   ##
-#######################################################################
-#######################################################################
+#!/usr/bin/env bash
+###################################################################
+###################################################################
+##                                                               ##
+## -------------INICIANDO INSTALAÇÃO NA SUA TV BOX-------------- ##
+##                                                               ##
+###################################################################
+###################################################################
 set -o errexit  # Exit script when a command exits with non-zero status
 set -o errtrace # Exit on error inside any functions or sub-shells
 set -o nounset  # Exit script on use of an undefined variable
@@ -14,11 +14,8 @@ set -o pipefail # Return exit status of the last command in the pipe that failed
 # ==============================================================================
 # GLOBALS
 # ==============================================================================
-readonly HOSTNAME="homeassistant"
-readonly OS_AGENT="os-agent_1.2.2_linux_aarch64.deb"
-readonly OS_AGENT_PATH="https://github.com/home-assistant/os-agent/releases/download/1.2.2/"
-readonly HA_INSTALLER="homeassistant-supervised.deb"
-readonly HA_INSTALLER_PATH="https://github.com/home-assistant/supervised-installer/releases/latest/download/"
+readonly HOSTNAME="armhassio"
+readonly HASSIO_INSTALLER="https://github.com/home-assistant/supervised-installer/releases/latest/download/"
 readonly REQUIREMENTS=(
   apparmor-utils
   apt-transport-https
@@ -45,9 +42,7 @@ update_hostname() {
     sed -i "s/${old_hostname}/${HOSTNAME}/g" /etc/hostname
     sed -i "s/${old_hostname}/${HOSTNAME}/g" /etc/hosts
     hostname "${HOSTNAME}"
-    echo ""
     echo "Hostname will be changed on next reboot: ${HOSTNAME}"
-    echo ""
   fi
 }
 
@@ -55,15 +50,11 @@ update_hostname() {
 # Installs all required software packages and tools
 # ------------------------------------------------------------------------------
 install_requirements() {
-  echo ""
   echo "Updating APT packages list..."
-  echo ""
-  apt-get --allow-releaseinfo-change update
   apt-get install software-properties-common
   apt-get update
-  echo ""
+
   echo "Ensure all requirements are installed..."
-  echo ""
   apt-get install -y "${REQUIREMENTS[@]}"
 }
 
@@ -71,9 +62,7 @@ install_requirements() {
 # Installs the Docker engine
 # ------------------------------------------------------------------------------
 install_docker() {
-  echo ""
   echo "Installing Docker..."
-  echo ""
   curl -fsSL https://get.docker.com | sh
 }
 
@@ -81,15 +70,8 @@ install_docker() {
 # Installs and starts Hass.io
 # ------------------------------------------------------------------------------
 install_hassio() {
-  echo ""
-  echo "Installing Home Assistant..."
-  echo ""
-  apt-get update
-  apt-get install udisks2 wget -y
-  wget "${OS_AGENT_PATH}${OS_AGENT}"
-  dpkg -i "${OS_AGENT}"
-  wget "${HA_INSTALLER_PATH}${HA_INSTALLER}"
-  dpkg -i "${HA_INSTALLER}"
+  echo "Installing Hass.io..."
+  curl -sL "${HASSIO_INSTALLER}" | bash -s -- -m qemuarm-64
 }
 
 # ------------------------------------------------------------------------------
@@ -102,17 +84,6 @@ config_network_manager() {
     echo -e "\n[connection]";
     echo "wifi.clone-mac-address=preserve";
   } >> "/etc/NetworkManager/NetworkManager.conf"
-}
-
-# ------------------------------------------------------------------------------
-# Upgrade final
-# ------------------------------------------------------------------------------
-upgrade_final() {
-  echo ""
-  echo "Upgrade..."
-  echo ""
-  sudo apt update
-  sudo apt upgrade -y
 }
 
 # ==============================================================================
@@ -133,16 +104,15 @@ main() {
   config_network_manager
   install_docker
   install_hassio
-  upgrade_final
 
   # Friendly closing message
   ip_addr=$(hostname -I | cut -d ' ' -f1)
   echo "======================================================================="
-  echo "Hass.io is now installing Home Assistant."
-  echo "This process may take up to 20 minutes. Please visit:"
-  echo "http://${HOSTNAME}.local:8123/ in your browser and wait"
-  echo "for Home Assistant to load."
-  echo "If the previous URL does not work, please try http://${ip_addr}:8123/"
+  echo "Esta sendo instalado o Home Assistant."
+  echo "Aguarde os 20 minutos, ok"
+  echo "Deu tudo certo! Parabens!!!"
+  echo "by Josiel, com ajuda do youtube,sites diversos e da comunidade Portuguesa HA"
+  echo "Para acessar va em: http://${ip_addr}:8123/"
 
   exit 0
 }
