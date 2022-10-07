3a468ec15168066bfe6942890dfa7de75eab3944
nsf1977 <34452549+nsf1977@users.noreply.github.com>
2022-10-08 00:16:48 +0100

    Update teste-armbian_install_script.sh

--- teste-armbian_install_script.sh
+++ teste-armbian_install_script.sh
@@ -2,7 +2,7 @@
 ###################################################################
 ###################################################################
 ##                                                               ##
-## -------------INICIANDO INSTALAÇÃO NA TV BOX-------------- ##
+## THIS SCRIPT SHOULD ONLY BE RUN ON A S9xxx BOX RUNNING ARMBIAN ##
 ##                                                               ##
 ###################################################################
 ###################################################################
@@ -14,10 +14,11 @@ set -o pipefail # Return exit status of the last command in the pipe that failed
 # ==============================================================================
 # GLOBALS
 # ==============================================================================
-readonly HOSTNAME="armhassio"
+readonly HOSTNAME="homeassistant"
 readonly OS_AGENT="os-agent_1.4.1_linux_aarch64.deb"
 readonly OS_AGENT_PATH="https://github.com/home-assistant/os-agent/releases/download/1.4.1/"
-readonly HASSIO_INSTALLER="https://github.com/home-assistant/supervised-installer/releases/latest/download/"
+readonly HA_INSTALLER="homeassistant-supervised.deb"
+readonly HA_INSTALLER_PATH="https://github.com/home-assistant/supervised-installer/releases/latest/download/"
 readonly REQUIREMENTS=(
   apparmor-utils
   apt-transport-https
@@ -44,7 +45,9 @@ update_hostname() {
     sed -i "s/${old_hostname}/${HOSTNAME}/g" /etc/hostname
     sed -i "s/${old_hostname}/${HOSTNAME}/g" /etc/hosts
     hostname "${HOSTNAME}"
+    echo ""
     echo "Hostname will be changed on next reboot: ${HOSTNAME}"
+    echo ""
   fi
 }
 
@@ -52,11 +55,15 @@ update_hostname() {
 # Installs all required software packages and tools
 # ------------------------------------------------------------------------------
 install_requirements() {
+  echo ""
   echo "Updating APT packages list..."
+  echo ""
+  apt-get --allow-releaseinfo-change update
   apt-get install software-properties-common
   apt-get update
-
+  echo ""
   echo "Ensure all requirements are installed..."
+  echo ""
   apt-get install -y "${REQUIREMENTS[@]}"
 }
 
@@ -64,7 +71,9 @@ install_requirements() {
 # Installs the Docker engine
 # ------------------------------------------------------------------------------
 install_docker() {
+  echo ""
   echo "Installing Docker..."
+  echo ""
   curl -fsSL https://get.docker.com | sh
 }
 
@@ -72,13 +81,16 @@ install_docker() {
 # Installs and starts Hass.io
 # ------------------------------------------------------------------------------
 install_hassio() {
-  echo "Installing Hass.io..."
+  echo ""
+  echo "Installing Home Assistant..."
+  echo ""
   apt-get update
   apt-get install udisks2 wget -y
   wget "${OS_AGENT_PATH}${OS_AGENT}"
   dpkg -i "${OS_AGENT}"
   wget "${HA_INSTALLER_PATH}${HA_INSTALLER}"
   dpkg -i "${HA_INSTALLER}"
+  apt --fix-broken install
 }
 
 # ------------------------------------------------------------------------------
@@ -91,6 +103,17 @@ config_network_manager() {
     echo -e "\n[connection]";
     echo "wifi.clone-mac-address=preserve";
   } >> "/etc/NetworkManager/NetworkManager.conf"
+}
+
+# ------------------------------------------------------------------------------
+# Upgrade final
+# ------------------------------------------------------------------------------
+upgrade_final() {
+  echo ""
+  echo "Upgrade..."
+  echo ""
+  sudo apt update
+  sudo apt upgrade -y
 }
 
 # ==============================================================================
@@ -116,11 +139,11 @@ main() {
   # Friendly closing message
   ip_addr=$(hostname -I | cut -d ' ' -f1)
   echo "======================================================================="
-  echo "Home Assistant está a ser instalado."
-  echo "Aguarde cerca de 20 minutos"
-  echo "Parabens! O Home Asistant foi instalado"
-  echo "Teste por nsf"
-  echo "Para aceder vá ao endereço: http://${ip_addr}:8123/"
+  echo "Hass.io is now installing Home Assistant."
+  echo "This process may take up to 20 minutes. Please visit:"
+  echo "http://${HOSTNAME}.local:8123/ in your browser and wait"
+  echo "for Home Assistant to load."
+  echo "If the previous URL does not work, please try http://${ip_addr}:8123/"
 
   exit 0
 }
